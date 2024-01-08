87d5a56d6ce68ad2272e477e83428471a2a28fef
sekidy20@gmail.com <sekidy20@gmail.comexitgit commit>
2024-01-08 09:12:36 -0800

    New line

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
-console.log(`C is fun
-Python is cool
-JavaScript is amazing`);
+const language1 = "C is fun";
+const language2 = "Python is cool";
+const language3 = "JavaScript is amazing";
+
+console.log(language1);
+console.log(language2);
+console.log(language3);
